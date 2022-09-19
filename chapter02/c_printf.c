5372d6b32d6066c212b86a2af8f4c02437855a51
parkseyeol <112923839+parkseyeol@users.noreply.github.com>
2022-09-19 10:48:32 +0900

    finish chapter01

---
+++ chapter02/c_printf.c
@@ -0,0 +1,15 @@
+#inlude <sutio.h>
+
+// 제어문자
+//   +  \n :한줄 개행
+//   +  \t :탭(8칸 스페이스)
+int main(void)
+{
+    /* code */
+    printf("HELLO\n");
+    printf("Chulwoong \t choi);
+    return 0;
+}
+
+
+     
--- intro/c_compile.c
+++ intro/c_compile.c
@@ -1,5 +1,24 @@
-#include <stdio.h>
+
+// 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include <stdio.h> //입력과 출력 관련 기능
 
 int main(void) {
+    int a /*학생번호*/, b /*학생이름*/;
     printf("hello c");
 }
+
+
